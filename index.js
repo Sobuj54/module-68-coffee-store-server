370dadda0dadddfda7a0f33ee9e58df795970b05
soubj <sobujahmed124@gmail.com>
2023-06-22 21:28:44 +0600

    successfully hidden username and password using env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,14 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// coffeeMaster
-// F5LPyoZsI151lHAM
-
-// const uri =
-//   "mongodb+srv://<username>:<password>@cluster0.l0lz8w0.mongodb.net/?retryWrites=true&w=majority";
-
-const uri =
-  "mongodb+srv://coffeeMaster:F5LPyoZsI151lHAM@cluster0.l0lz8w0.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l0lz8w0.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
